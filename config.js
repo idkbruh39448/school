ca02873856861206826d126d4ed8b13fabb3427b
idkbruh39448 <zyhir112@gmail.com>
2025-05-06 16:31:24 -0400

    Initial commit

---
+++ config.js
@@ -0,0 +1,14 @@
+const config = {
+  challenge: true, // Set to true if you want to enable password protection.
+  blocked: {
+    // Normal users can ignore this option. This is for the owner of the website.
+    "gointerstellar.app": "",
+    localhost: "",
+  },
+  users: {
+    // You can add multiple users by doing username: 'password'.
+    interstellar: "2009",
+  },
+};
+
+export default config;
